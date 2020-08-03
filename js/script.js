5bba5ff7b1905fd0bce251904892af672addbbc8
Cornelius Range V <cfr2116@columbia.edu>
2020-08-03 02:19:31 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,17 +8,6 @@ $(document).ready(function() {
 			$('.borboletas').fadeOut(1500);
 		}, 1500);
 
-		// setTimeout(function(){
-		// 	$('#project-title').show();
-		// 	$('#artist-name').show();
-		// 	$('#bars').show();
-		// }, 3500);
-    //
-    //
-		// $( "#skip" ).click(function() {
-		// 	$('#project-title').hide();
-		// 	$('#bars').hide();
-		// 	$('#artist-name').hide();
 
 		setTimeout(function(){
 			$('body').removeClass("noscroll");
@@ -31,7 +20,7 @@ $(document).ready(function() {
 			s.type = "text/javascript";
 			s.src = "//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5eeed4279d4dd544";
 			$("body").append(s);
-		}, 5000)
+		}, 5000);
  });
 
 
@@ -41,14 +30,5 @@ $(window).on('beforeunload', function() {
 });
 
 
-	//   setTimeout(function(){
-	// 		$('#project-title').hide();
-	// 		$('#artist-name').hide();
-	//   }, 6000);
-  //
-  //
-  // setTimeout(function(){
-	// 	  $('body').addClass('loaded');
-	// 		$('body').removeClass("noscroll");
-	// 	}, 6400);
- });
+
+
